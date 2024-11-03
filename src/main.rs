12599f32add96c1c069d879e93dd5e3a9f9dfb08
Tox <toxikuu@users.noreply.github.com>
2024-11-03 16:35:35 -0600

    stabs now works with rid's versions

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-// use reqwest::blocking::Client;
+use rayon::prelude::*;
 use ureq::get;
 use scraper::{Html, Selector};
 use std::error::Error;
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Write, BufRead, BufReader};
+use std::fs::File;
+use std::io::{Read, BufRead, BufReader};
 use std::collections::HashMap;
+use std::env;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct Package {
     name: String,
-    url: String,
+    upstream: String,
     selector: Option<String>,
 }
 
@@ -37,16 +38,16 @@ fn determine_default_selector(url: &str) -> Option<&str> {
 }
 
 fn latest(pkg: &Package) -> Result<String, Box<dyn Error>> {
-    // let client = Client::builder()
-    //     .user_agent("tabs")
-    //     .build()?;
-    //
-    // let response = client.get(&pkg.url).send()?.text()?;
-    let response = get(&pkg.url).set("User-Agent", "tabs").call()?.into_string()?;
+
+    if pkg.upstream.is_empty() {
+        return Err(format!("Empty upstream for {}", pkg.name).into())
+    }
+
+    let response = get(&pkg.upstream).set("User-Agent", "stabs").call()?.into_string()?;
     let document = Html::parse_document(&response);
 
     let selector: Selector;
-    let default_selector = determine_default_selector(&pkg.url);
+    let default_selector = determine_default_selector(&pkg.upstream);
 
     if let Some(selector_str) = pkg.selector.as_deref().or(default_selector) {
         selector = Selector::parse(selector_str).unwrap();
@@ -59,11 +60,16 @@ fn latest(pkg: &Package) -> Result<String, Box<dyn Error>> {
         if let Some(version) = element.text().next() {
             let mut vers = version.trim().to_string().to_lowercase();
 
-            // println!("vers: {}", vers);
-
             // version string cleanup
             if vers.contains(&pkg.name) {
                 vers = vers.replace(&pkg.name, "").trim().to_string();
+            }
+
+            if vers.contains('-') {
+                let alt_name = pkg.name.replace('_', "-");
+                if vers.contains(&alt_name) {
+                    vers = vers.replace(&alt_name, "").trim().to_string()
+                }
             }
 
             if vers.starts_with('v') {
@@ -93,36 +99,27 @@ fn latest(pkg: &Package) -> Result<String, Box<dyn Error>> {
     Err(format!("Failed to find the latest version for {}", pkg.name).into())
 }
 
-fn snapshot(versions: &HashMap<String, String>) -> Result<(), Box<dyn Error>> {
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open("versions.txt")?;
-    for (name, version) in versions {
-        writeln!(file, "{}={}", name, version)?;
-    }
-    Ok(())
-}
-
-fn read_snapshot() -> HashMap<String, String> {
+fn read_versions(file_path: &str) -> HashMap<String, String> {
     let mut versions = HashMap::new();
-    if let Ok(file) = File::open("versions.txt") {
+    if let Ok(file) = File::open(file_path) {
         let reader = BufReader::new(file);
 
         for line in reader.lines().map_while(Result::ok) {
-            let parts: Vec<&str> = line.split('=').collect();
-            if parts.len() == 2 {
-                versions.insert(parts[0].to_string(), parts[1].to_string());
+            if let Some(stripped) = line.strip_prefix("export") {
+                let parts: Vec<&str> = stripped.split("_version=").collect();
+                if parts.len() == 2 {
+                    let name = parts[0].trim().to_string();
+                    let version = parts[1].trim().trim_matches('"').to_string();
+                    versions.insert(name, version);
+                }
             }
         }
     }
-
     versions
 }
 
-fn read_json() -> Result<Vec<Package>, Box<dyn Error>> {
-    let mut file = File::open("tabs.json")?;
+fn read_json(file_path: &str) -> Result<Vec<Package>, Box<dyn Error>> {
+    let mut file = File::open(file_path)?;
     let mut json_string = String::new();
     file.read_to_string(&mut json_string)?;
 
@@ -131,40 +128,29 @@ fn read_json() -> Result<Vec<Package>, Box<dyn Error>> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let packages = read_json()?;
-    let saved_versions = read_snapshot();
-    let mut current_versions = HashMap::new();
+    let json_path = env::var("STABS_JSON").unwrap_or_else(|_| "/etc/rid/pkgs.json".to_string());
+    let packages = read_json(&json_path)?;
+    let env_versions = read_versions("/etc/rid/versions");
 
-    for pkg in packages {
-        match latest(&pkg) {
+    packages.par_iter().for_each(|pkg| {
+        match latest(pkg) {
             Ok(version) => {
 
-                let change = if let Some(saved_version) = saved_versions.get(&pkg.name) {
-                    if saved_version != &version {
-                        format!("\x1b[31m{}\x1b[0m -> ", saved_version)
+                if let Some(env_version) = env_versions.get(&pkg.name) {
+                    if version != *env_version {
+                        let displayed_version = format!("\x1b[31;1m{}\x1b[0m", version);
+                        println!("{}: {} <-> {}", pkg.name, env_version, displayed_version);
                     } else {
-                        String::new()
+                        println!("{}: {} <-> {}", pkg.name, env_version, version);
                     }
-                } else {
-                    String::from("\x1b[31m(new)\x1b[0m")
-                };
+                }
 
-                let displayed_version = if change.contains("(new)") {
-                    format!("{} {}", version, change)
-                } else if change.contains("->") {
-                    format!("{} {}", change, version)
-                } else {
-                    version.clone()
-                };
-
-                println!("{:<16} = {}", pkg.name, displayed_version);
-
-                current_versions.insert(pkg.name.clone(), version);
             }
-            Err(e) => eprintln!("Error: {}", e)
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
         }
-    }
+    });
 
-    snapshot(&current_versions)?;
     Ok(())
 }
